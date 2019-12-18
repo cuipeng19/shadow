601cdd1a1aaf465e9e724fa765ca3464764754e1
cuipeng <cuipengb@yonyou.com>
2019-12-18 15:22:49 +0800

    eureka

---
+++ eureka-server/src/main/java/com/shadow/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.shadow.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author cuipeng 2019/12/17 11:01
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
