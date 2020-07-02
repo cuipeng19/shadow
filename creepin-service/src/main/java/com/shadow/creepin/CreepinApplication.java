0a302e396f876249ad7ee9126c7faf8ee37f977c
cuipeng <cuipengb@yonyou.com>
2020-07-02 17:11:07 +0800

    add mapper

--- creepin-service/src/main/java/com/shadow/creepin/CreepinApplication.java
+++ creepin-service/src/main/java/com/shadow/creepin/CreepinApplication.java
@@ -3,12 +3,14 @@ package com.shadow.creepin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author cuipeng 2019/12/19 10:50
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan(basePackages = "com.shadow.creepin.dao")
 public class CreepinApplication {
 
     public static void main(String[] args) {
