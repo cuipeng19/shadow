04b7fa4aab5645e2a340da2813e584990820312e
cuipeng <cuipengb@yonyou.com>
2020-07-07 16:54:23 +0800

    add letter-service

---
+++ letter-service/src/main/java/com.shadow.letter/LetterApplication.java
@@ -0,0 +1,15 @@
+package com.shadow.letter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cuipeng 2020/7/7 16:45
+ */
+@SpringBootApplication
+public class LetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetterApplication.class, args);
+    }
+}
