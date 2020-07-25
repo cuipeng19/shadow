770acca84dac65066cc8a4a652e1d0da78eb7c8b
cuipeng <cuipengb@yonyou.com>
2020-07-25 17:12:54 +0800

    add token bucket

--- stan-service-gateway/src/main/java/com/shadow/stan/gateway/StanGatewayApplication.java
+++ stan-service-gateway/src/main/java/com/shadow/stan/gateway/StanGatewayApplication.java
@@ -2,6 +2,9 @@ package com.shadow.stan.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 /**
  * @author cuipeng 2020/7/21 10:19
@@ -12,4 +15,10 @@ public class StanGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(StanGatewayApplication.class, args);
     }
+
+    @Bean
+    public KeyResolver uriKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getPath().toString());
+    }
+
 }
