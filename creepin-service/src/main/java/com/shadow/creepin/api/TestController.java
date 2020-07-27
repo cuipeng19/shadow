6b854562d0db064e0bc8fd90a5ee0c0fcfe0f56c
cuipeng <cuipengb@yonyou.com>
2020-07-27 16:23:10 +0800

    add redis

--- creepin-service/src/main/java/com/shadow/creepin/api/TestController.java
+++ creepin-service/src/main/java/com/shadow/creepin/api/TestController.java
@@ -5,12 +5,14 @@ import com.shadow.common.bean.creepin.ao.TestAO;
 import com.shadow.creepin.service.TestService;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author cuipeng 2019/12/19 11:12
@@ -21,15 +23,18 @@ public class TestController {
 
     @Autowired
     private TestService testService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
 
     @GetMapping("/test")
     public Object test(@Validated @RequestBody TestAO ao) {
 
-        Object o = testService.test(ao);
+//        Object o = testService.test(ao);
+        stringRedisTemplate.opsForValue().set("creepin","aaa", 1, TimeUnit.MINUTES);
+        System.out.println(stringRedisTemplate.opsForValue().get("creepin"));
 
-
-        return o;
+        return ResultDTO.success();
     }
 
 }
