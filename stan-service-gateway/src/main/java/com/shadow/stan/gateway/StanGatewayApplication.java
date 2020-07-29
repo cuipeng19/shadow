5e73de89b257bf636b7f2f78ddea08f75e985371
cuipeng <cuipengb@yonyou.com>
2020-07-29 17:33:02 +0800

    modify request body

--- stan-service-gateway/src/main/java/com/shadow/stan/gateway/StanGatewayApplication.java
+++ stan-service-gateway/src/main/java/com/shadow/stan/gateway/StanGatewayApplication.java
@@ -3,7 +3,10 @@ package com.shadow.stan.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import reactor.core.publisher.Mono;
 
 /**
@@ -21,4 +24,11 @@ public class StanGatewayApplication {
         return exchange -> Mono.just(exchange.getRequest().getPath().toString());
     }
 
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("rewrite_request_obj", r -> r.host("*.rewriterequestobj.org")
+                        .filters(f -> f.prefixPath("/httpbin")
+                                .modifyRequestBody(String.class, String.class, MediaType.APPLICATION_JSON_VALUE, );
+    }
 }
