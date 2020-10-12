e942d271253a4d10c84e8f1b4e129fb1cefc9472
cuipeng <cuipengb@yonyou.com>
2020-10-12 16:02:13 +0800

    add algorithm

---
+++ creepin-service/src/main/java/com/shadow/creepin/api/AlgorithmController.java
@@ -0,0 +1,60 @@
+package com.shadow.creepin.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author cuipeng 2020/10/12 11:24
+ */
+public class AlgorithmController {
+
+    /**
+     * 两数之和
+     * 使用哈希表寻找target-x的时间复杂度为O(1)
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{(int) map.get(target-nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    /**
+     * 整数反转
+     * x%10弹出原数字末尾，result*10 + x%10推入新数字末尾
+     */
+    public int reverse(int x) {
+        int result = 0;
+        while(x!=0) {
+            if((result * 10 / 10)!=result) {
+                result = 0;
+                break;
+            }
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+        return result;
+    }
+
+    /**
+     * 回文数
+     * 反转一半数字，原数字<=新数字则结束
+     * 原数字<0 或 非0且末尾=0，返回否
+     */
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x!=0 && x%10==0)) {
+            return false;
+        }
+        int result = 0;
+        while (x>result) {
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+        return x==result || x==result/10;
+    }
+
+}
