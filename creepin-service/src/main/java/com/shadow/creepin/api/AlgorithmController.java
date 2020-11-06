ae89aa00845d2ae1262c125982a8e1ddd5f787a6
cuipeng <cuipengb@yonyou.com>
2020-11-06 17:44:43 +0800

    add plus one

--- creepin-service/src/main/java/com/shadow/creepin/api/AlgorithmController.java
+++ creepin-service/src/main/java/com/shadow/creepin/api/AlgorithmController.java
@@ -390,4 +390,25 @@ public class AlgorithmController {
         return count;
     }
 
+
+    /**
+     * 加一
+     * 是9改0，否则加1，全9新建数组首位为1
+     */
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--) {
+            if(digits[i]==9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+
+        digits = new int[digits.length+1];
+        digits[0] = 1;
+        return digits;
+    }
+
+
 }
