76a44f390a9830ca57ea2070bb4e3f4e8cb6001a
cuipeng <cuipengb@yonyou.com>
2021-01-11 11:16:04 +0800

    add invertTree

--- creepin-service/src/main/java/com/shadow/creepin/api/AlgorithmController.java
+++ creepin-service/src/main/java/com/shadow/creepin/api/AlgorithmController.java
@@ -1502,4 +1502,21 @@ public class AlgorithmController {
         }
     }
 
+
+    /**
+     * 翻转二叉树
+     * 递归
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
 }
