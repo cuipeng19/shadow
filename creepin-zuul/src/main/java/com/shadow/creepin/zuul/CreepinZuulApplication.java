b8cc9754679ed6efac23ed7d340c828d155d7292
cuipeng <cuipengb@yonyou.com>
2021-04-01 14:33:42 +0800

    add zuul

---
+++ creepin-zuul/src/main/java/com/shadow/creepin/zuul/CreepinZuulApplication.java
@@ -0,0 +1,16 @@
+package com.shadow.creepin.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cuipeng 2021/3/30 16:25
+ */
+@SpringBootApplication
+public class CreepinZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreepinZuulApplication.class, args);
+    }
+
+}
